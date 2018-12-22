63df06df7f9f81643a58f34a28ed6dae991e7668
jiraspiom <jiraspiom@yahoo.com.br>
2018-12-22 09:30:55 -0200

    Criado projeto de alerta

--- Alerta/ViewController.swift
+++ Alerta/ViewController.swift
@@ -9,7 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func AbrirAlerta(_ sender: Any) {
+        //cria o objeto
+        let alerta = UIAlertController(title: "Adicionar contato", message: "Deseja adicionar um contato?", preferredStyle: .alert)
+        
+        //configurao o objeto
+        let confirmar = UIAlertAction(title: "Confirmar", style: .default, handler: nil)
+        let cancelar = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        alerta.addAction(confirmar)
+        alerta.addAction(cancelar)
+        
+        //mostra acao do objeto
+        present(alerta, animated: true, completion: nil)
+        
+    }
+    @IBAction func AbrirAlerta2(_ sender: Any) {
+    
+        let alerta = UIAlertController(title: "Remover Contato", message: "Deseja remover contato", preferredStyle: .actionSheet)
+    
+        let confirmar = UIAlertAction(title: "Confirmar", style: .default, handler: nil)
+        let cancelar = UIAlertAction(title: "Cancelar", style: .destructive, handler: nil)
+        
+        alerta.addAction(confirmar)
+        alerta.addAction(cancelar)
+        
+        present(alerta, animated: true, completion: nil)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
